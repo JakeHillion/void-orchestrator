0e063340572847c05481948ea864b7d1afd07e1e
Jake Hillion <jake@hillion.co.uk>
2022-05-04 12:50:21 +0100

    updated clap

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,43 @@ use clone_shim::{run, RunArgs};
 
 use std::path::Path;
 
-use clap::{App, AppSettings};
+use clap::{Arg, Command};
 
 fn main() {
     // process arguments
-    let matches = App::new("clone-shim")
+    let matches = Command::new("clone-shim")
         .version(env!("GIT_HASH"))
         .author("Jake Hillion <jake@hillion.co.uk>")
-        .about("Launch a multi entrypoint app, cloning as requested by an external specification or the ELF.")
-        .arg(clap::Arg::new("spec").long("specification").short('s').help("Provide the specification as an external JSON file.").takes_value(true))
-        .setting(AppSettings::TrailingVarArg)
-        .arg(clap::Arg::new("verbose").long("verbose").short('v').help("Use verbose logging.").takes_value(false))
-        .arg(clap::Arg::new("debug").long("debug").short('d').help("Stop each spawned application process so that it can be attached to.").takes_value(false))
-        .arg(clap::Arg::new("binary").index(1).help("Binary and arguments to launch with the shim").required(true).multiple_values(true))
+        .about("Launch a void process application.")
+        .trailing_var_arg(true)
+        .arg(
+            Arg::new("spec")
+                .long("specification")
+                .short('s')
+                .help("Provide the specification as an external JSON file.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("verbose")
+                .long("verbose")
+                .short('v')
+                .help("Use verbose logging.")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::new("debug")
+                .long("debug")
+                .short('d')
+                .help("Stop each spawned application process so that it can be attached to.")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::new("binary")
+                .index(1)
+                .help("Binary and arguments to launch with the shim")
+                .required(true)
+                .multiple_values(true),
+        )
         .get_matches();
 
     // setup logging
