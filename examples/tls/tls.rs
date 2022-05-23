71888d40e71be0c217612319181a6a8ccd9dc35f
Jake Hillion <jake@hillion.co.uk>
2022-05-23 19:42:07 +0100

    added tls processing

--- examples/tls/tls.rs
+++ examples/tls/tls.rs
@@ -1,20 +1,28 @@
 use std::fs::File;
-use std::io::{self, ErrorKind, Read, Write};
+use std::io::{self, BufReader, ErrorKind, Read, Write};
 use std::net::TcpStream;
 use std::os::unix::io::AsRawFd;
 use std::os::unix::net::UnixStream;
+use std::sync::Arc;
 
 use nix::poll::{poll, PollFd, PollFlags};
+
+use rustls::ServerConnection;
+
+use anyhow::Context;
 
 const BUFFER_SIZE: usize = 4096;
 
 pub(crate) fn handler(
     http_trigger_socket: File,
-    _cert: File,
-    _key: File,
+    cert: File,
+    key: File,
     mut stream: TcpStream,
 ) -> i32 {
     let (mut socket, far_socket) = UnixStream::pair().unwrap();
+
+    let config = make_config(cert, key);
+    let mut tls_conn = rustls::ServerConnection::new(config).unwrap();
 
     super::http_handler(&http_trigger_socket, far_socket);
 
@@ -32,13 +40,13 @@ pub(crate) fn handler(
 
         if let Some(events) = to_poll[0].revents() {
             if events.contains(PollFlags::POLLIN) {
-                handle_encrypted_data(&mut stream, &mut socket).unwrap();
+                handle_encrypted_data(&mut tls_conn, &mut stream, &mut socket).unwrap();
             }
         }
 
         if let Some(events) = to_poll[1].revents() {
             if events.contains(PollFlags::POLLIN) {
-                handle_new_data(&mut socket, &mut stream).unwrap();
+                handle_new_data(&mut tls_conn, &mut socket, &mut stream).unwrap();
             }
 
             if events.contains(PollFlags::POLLHUP) {
@@ -51,29 +59,61 @@ pub(crate) fn handler(
     exitcode::OK
 }
 
-fn handle_encrypted_data(stream: &mut impl Read, socket: &mut impl Write) -> io::Result<()> {
-    let mut buf = [0_u8; BUFFER_SIZE];
+fn handle_encrypted_data(
+    tls_conn: &mut ServerConnection,
+    stream: &mut (impl Read + Write),
+    socket: &mut impl Write,
+) -> anyhow::Result<()> {
+    println!("handling newly received encrypted data");
 
     loop {
-        let read = non_blocking_read(stream, &mut buf)?;
+        let read = match tls_conn.read_tls(stream) {
+            Err(e) => {
+                if e.kind() == ErrorKind::WouldBlock {
+                    0
+                } else {
+                    return Err(e).context("io error reading from stream");
+                }
+            }
+            Ok(n) => n,
+        };
+
         if read == 0 {
             return Ok(());
         }
 
-        socket.write_all(&buf[0..read]).unwrap();
+        let process_result = tls_conn.process_new_packets();
+        let write_tls_result = tls_conn.write_tls(stream);
+
+        let io_state = process_result.context("tls processing failure")?;
+        write_tls_result.context("tls write failure")?;
+
+        if io_state.plaintext_bytes_to_read() > 0 {
+            let mut reader = tls_conn
+                .reader()
+                .take(io_state.plaintext_bytes_to_read() as u64);
+
+            std::io::copy(&mut reader, socket)?;
+        }
     }
 }
 
-fn handle_new_data(socket: &mut impl Read, stream: &mut impl Write) -> io::Result<()> {
-    let mut buf = [0_u8; BUFFER_SIZE];
+fn handle_new_data(
+    tls_conn: &mut ServerConnection,
+    socket: &mut impl Read,
+    stream: &mut impl Write,
+) -> anyhow::Result<()> {
+    println!("handling new data to encrypt");
 
+    let mut buf = [0_u8; BUFFER_SIZE];
     loop {
         let read = non_blocking_read(socket, &mut buf)?;
         if read == 0 {
             return Ok(());
         }
 
-        stream.write_all(&buf[0..read]).unwrap();
+        tls_conn.writer().write_all(&buf[0..read])?;
+        tls_conn.write_tls(stream)?;
     }
 }
 
@@ -88,4 +128,46 @@ fn non_blocking_read(reader: &mut impl io::Read, buf: &mut [u8]) -> io::Result<u
         }
         Ok(n) => Ok(n),
     }
+}
+
+fn make_config(cert: File, key: File) -> Arc<rustls::ServerConfig> {
+    let certs = load_certs(cert);
+    let privkey = load_private_key(key);
+
+    let config = rustls::ServerConfig::builder()
+        .with_safe_default_cipher_suites()
+        .with_safe_default_kx_groups()
+        .with_safe_default_protocol_versions()
+        .expect("inconsistent cipher-suites/versions specified")
+        .with_no_client_auth()
+        .with_single_cert(certs, privkey)
+        .expect("bad certificates/private key");
+
+    Arc::new(config)
+}
+
+fn load_certs(certfile: File) -> Vec<rustls::Certificate> {
+    let mut reader = BufReader::new(certfile);
+
+    rustls_pemfile::certs(&mut reader)
+        .unwrap()
+        .iter()
+        .map(|v| rustls::Certificate(v.clone()))
+        .collect()
+}
+
+fn load_private_key(keyfile: File) -> rustls::PrivateKey {
+    let mut reader = BufReader::new(keyfile);
+
+    loop {
+        match rustls_pemfile::read_one(&mut reader).expect("cannot parse private key .pem file") {
+            Some(rustls_pemfile::Item::RSAKey(key)) => return rustls::PrivateKey(key),
+            Some(rustls_pemfile::Item::PKCS8Key(key)) => return rustls::PrivateKey(key),
+            Some(rustls_pemfile::Item::ECKey(key)) => return rustls::PrivateKey(key),
+            None => break,
+            _ => {}
+        }
+    }
+
+    panic!("no keys found (encrypted keys not supported)");
 }
